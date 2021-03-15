0b715ea1f6b778350fac13bf8e322c02721c086d
Phan Hoàng Kỳ <phanhoangky1412@gmail.com>
2021-03-15 09:10:49 +0700

    config

--- config/config.ts
+++ config/config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
   history: {
     type: 'browser',
   },
+  publicPath: "/welcome",
   locale: {
-    // default zh-CN
     default: 'en-US',
     antd: true,
     // default true, when it is true, will use `navigator.language` overwrite default
@@ -27,7 +27,8 @@ export default defineConfig({
   targets: {
     ie: 11,
   },
-  base: "/welcome",
+  base: "/",
+
   // umi routes: https://umijs.org/docs/routing
   routes,
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
